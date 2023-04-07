535739291d69fe71c07d621d09c3758041619afa
Kevin Hintz <khintz34@gmail.com>
2023-04-07 14:00:44 -0500

    new init

---
+++ src/assets/dataList.js
@@ -0,0 +1,86 @@
+export const SearchResults = [
+  {
+    search: "Apple",
+    clicked: false,
+  },
+  {
+    search: "Mac",
+    clicked: false,
+  },
+  {
+    search: "David Beckham",
+    clicked: true,
+  },
+  {
+    search: "Beckham",
+    clicked: false,
+  },
+  {
+    search: "Zebra",
+    clicked: true,
+  },
+  {
+    search: "Apps",
+    clicked: true,
+  },
+  {
+    search: "Becks",
+    clicked: false,
+  },
+  {
+    search: "Boris Becker",
+    clicked: true,
+  },
+  {
+    search: "Computer",
+    clicked: false,
+  },
+  {
+    search: "David Dunk",
+    clicked: false,
+  },
+  {
+    search: "Masters",
+    clicked: false,
+  },
+  {
+    search: "Golf",
+    clicked: false,
+  },
+  {
+    search: "Soccer",
+    clicked: false,
+  },
+  {
+    search: "Hockey",
+    clicked: true,
+  },
+  {
+    search: "Sports",
+    clicked: false,
+  },
+  {
+    search: "Rahm",
+    clicked: true,
+  },
+  {
+    search: "Scottie",
+    clicked: false,
+  },
+  {
+    search: "Hads",
+    clicked: false,
+  },
+  {
+    search: "Sam Bennet",
+    clicked: true,
+  },
+  {
+    search: "Sam Pausha",
+    clicked: false,
+  },
+  {
+    search: "Augusta",
+    clicked: true,
+  },
+];
---
+++ src/pages/_search.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import { SearchResults } from "../assets/dataList";
+// import "@/styles/Search.module.css";
+
+const Search = () => {
+  let newList = SearchResults.sort((a, b) => {
+    if (a.clicked === b.clicked) {
+      return a.search < b.search ? -1 : 1;
+    } else {
+      return a.clicked < b.clicked ? 1 : -1;
+    }
+  });
+
+  let searchedArray = SearchResults.filter(function (el) {
+    return el.clicked === true;
+  });
+
+  const [showSearch, setShowSearch] = useState("hide");
+  const [searchVal, setSearchVal] = useState("");
+  const [resultsList, setResultsList] = useState(searchedArray);
+  const [masterList, setMasterList] = useState(newList);
+  const [searching, setSearching] = useState(false);
+  const [searchList, setSearchList] = useState([]);
+  const [firstSearch, setFirstSearch] = useState("show");
+
+  function searchForResults(val) {
+    if (val === "") {
+      setResultsList(searchedArray);
+      return;
+    }
+
+    let array = [];
+    let split = val.split("");
+
+    masterList.map((result, index) => {
+      let splitResult = result.search.toLowerCase().split("");
+      let length = split.length;
+      let current = splitResult.slice(0, length);
+      if (
+        current.length === split.length &&
+        current.every((value, index) => value === split[index])
+      ) {
+        array.push(result);
+      }
+    });
+
+    setResultsList([...array]);
+    // setMasterList([...array]);
+  }
+
+  useEffect(() => {
+    if (searchVal === "") {
+      setFirstSearch("show");
+    }
+  });
+
+  return (
+    <div id="searchContainer">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="searchBar"
+        onClick={() => {
+          setShowSearch("show");
+        }}
+        value={searchVal}
+        onChange={(e) => {
+          searchForResults(e.target.value);
+          setSearchVal(e.target.value);
+          setFirstSearch("hide");
+        }}
+      />
+      <div>
+        <ul className={`searchResults ${showSearch}`}>
+          <p className={firstSearch}>Related to your recent searches</p>
+          {resultsList.map((val, index) => {
+            return <li key={`${val.search}-li`}>{val.search}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
